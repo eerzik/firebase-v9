97eb3c10c3ed6c1b9d86e5478d03bb6969fad72c
Engin ERZİK <erzik.engin@gmail.com>
2023-06-14 15:31:10 +0300

    Belge Silme İşlemi

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,13 @@
+import { db } from '../firebase/config'
+import { doc, deleteDoc } from '@firebase/firestore'
+
 
 export default function BookList({ books }) {
     const handleClick = async (id) => {
-        console.log(id)
+       // console.log(id)
+
+       const ref =doc(db,'books',id);
+       await deleteDoc(ref);
     }
     return (
         <div className="book-list" >
