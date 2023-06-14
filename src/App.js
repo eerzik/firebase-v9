09484ad27a59e7d42ff29492f0fe3ac8cf0ce8ab
Engin ERZİK <erzik.engin@gmail.com>
2023-06-14 17:38:35 +0300

    Route Guart işlemi

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
   return (
     <div className="App">
-      <Router>
-          <Navbar />   
-            <Routes>
-              <Route path="/" element={<Home />}> </Route>
-              <Route path="/signup" element={<Signup />}> </Route>
-              <Route path="/login" element={<Login />}>  </Route>
-            </Routes>
-      </Router>
+      {authIsReady && (
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={user ? <Home /> : <Login />}> </Route>
+            <Route path="/signup" element={user ? <Home /> : <Signup />}> </Route>
+            <Route path="/login" element={user ? <Home /> : <Login />}>  </Route>
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
