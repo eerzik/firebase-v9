1de096ec0695520d4412195af70fa52759bb7464
Engin ERZİK <erzik.engin@gmail.com>
2023-06-14 15:22:20 +0300

    Belgelere gerçek zamanlı erişim

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,27 @@
-import { useState, useEffect } from "react";
+
 import BookList from "../components/BookList";
 import BookForm from "../components/BookForm";
 
-import { db } from "../firebase/config";
-import { collection, getDocs } from "firebase/firestore"
+import { useCollection } from "../hooks/useCollection";
 
 export default function Home() {
 
-    const [books, setBooks] = useState(null)
-    useEffect(() => {
-        const ref = collection(db, 'books');
-        getDocs(ref)
-            .then((snap) => {
-                let result=[];
-                snap.forEach(doc=>{
-                    //... nokta kullandığımızda içerideki tüm dataları çekmiş oluyoruz.
-                    result.push({id:doc.id,...doc.data()})
-                })
-                setBooks(result);
-            })
+    const {documents : books}= useCollection('books')
 
-    }, [])
+    // const [books, setBooks] = useState(null)
+    // useEffect(() => {
+    //     const ref = collection(db, 'books');
+    //     getDocs(ref)
+    //         .then((snap) => {
+    //             let result=[];
+    //             snap.forEach(doc=>{
+    //                 //... nokta kullandığımızda içerideki tüm dataları çekmiş oluyoruz.
+    //                 result.push({id:doc.id,...doc.data()})
+    //             })
+    //             setBooks(result);
+    //         })
+
+    // }, [])
 
     return (
         <div className="App" >
