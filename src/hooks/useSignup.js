eb7e8961be4ffd40d315fb049d4c04b142b8daf9
Engin ERZİK <erzik.engin@gmail.com>
2023-06-14 16:31:56 +0300

    Kullanıcı verilerini dispatch etme işlemi

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { auth } from "../firebase/config";
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
     const [error, setError] = useState(null);
+    const{dispatch}=useAuthContext();
     const signup = (email, password) => {
         setError(null)
         createUserWithEmailAndPassword(auth, email, password)
             .then((res) => {
-                console.log('kullanıcı oluştu' , res.user);
+                //console.log('kullanıcı oluştu' , res.user);
+                dispatch({type:'LOGIN',payload:res.user});
             })
             .catch((err) => {
                 setError(err.message)
